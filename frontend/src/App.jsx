4d1a59d970a3000d6bbd18fd34b623fd8fe65053
Adnaan1806 <CODSE221F-067@student.nibm.lk>
2024-04-17 01:28:26 +0530

    create post page update with Add button category backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import PostDetails from "./pages/PostDetails";
+import CreatePost from "./pages/CreatePost";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/register" element={<Register />} />
         <Route exact path="/posts/post/:id" element={<PostDetails />} />
+        <Route exact path="/write" element={<CreatePost />} />
       </Routes>
   
     </div>

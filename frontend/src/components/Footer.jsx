3a86f22a161ea9f2f0fc68b30b1dfb471ce40add
Adnaan1806 <CODSE221F-067@student.nibm.lk>
2024-04-16 21:39:06 +0530

    homepost page update

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,19 @@
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="mt-8 w-full bg-black px-8 md:px-[500px] flex justify-between text-sm md:text-md py-8">
+        <div className="flex flex-col text-white">
+            <p>Featured Blogs</p>
+            <p>Most viewed</p>
+            <p>Readers Choice</p>
+        </div>
+
+        <div className="flex flex-col text-white">
+            <p>Forum</p>
+            <p>Support</p>
+            <p>Recent Posts</p>
+        </div>
+    </div>
   )
 }
 
--- frontend/src/components/HomePosts.jsx
+++ frontend/src/components/HomePosts.jsx
@@ -1,8 +1,36 @@
-
+import logo from '../assets/growtika-nGoCBxiaRO0-unsplash.jpg'
 
 const HomePosts = () => {
   return (
-    <div>HomePosts</div>
+    <div className="w-full flex mt-8 space-x-4">
+
+       {/*left*/}
+        <div className="w-[35%] h-[200px] flex justify-center items-center">
+            <img src={logo} alt="" className="h-full w-full object-cover"/>
+
+        </div>
+
+       {/*right*/}
+        <div className="flex flex-col w-[65%]">
+           <h1 className="text-xl font-bold md:mb-2 mb-1 md:text-2xl">
+            10 Uses of Artificial Intelligence in Day to Day Life
+           </h1>
+           <div className="flex mb-2 text-sm font-semibold text-gray-500 items-center justify-between md:mb-4">
+              <p>@snehasishdev</p>
+              <div className="flex space-x-2">
+                    <p>16/06/2023</p>
+                    <p>16:45</p>
+              </div>
+           </div>
+           <p className="text-sm md:text-lg">
+            Prominent examples of AI software used in everyday life include Google Maps, Netflix, and Siri. These applications use 
+            AI to provide personalized recommendations, predict user behavior, and improve the user experience. AI is also used in
+            healthcare, finance, and transportation to automate processes, analyze data, and make predictions. Overall, AI has the
+            potential to transform various industries and improve the quality of life for people around the world.
+           </p>
+        </div>
+
+    </div>
   )
 }
 

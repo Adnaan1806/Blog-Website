361135372c1bb6e6434af1367324d6ba9f234ae9
Adnaan1806 <CODSE221F-067@student.nibm.lk>
2024-04-17 10:23:56 +0530

    minor frontend changes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import Footer from "../components/Footer";
 
 const Login = () => {
   return (
@@ -35,6 +36,7 @@ const Login = () => {
           </div>
         </div>
       </div>
+      <Footer/>
     </>
   );
 };

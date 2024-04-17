a1b48645d01e40aee9f975731e141684b4c2616a
Adnaan1806 <CODSE221F-067@student.nibm.lk>
2024-04-17 18:36:30 +0530

    user routes update

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 
 //database
 const connectDB = async () => {
@@ -18,6 +19,7 @@ const connectDB = async () => {
 dotenv.config();
 app.use(express.json());
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 app.listen(5000, () => {
   connectDB();

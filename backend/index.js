a77b8093794d7b60932f24056ad415add7c85f8c
Adnaan1806 <CODSE221F-067@student.nibm.lk>
2024-04-17 11:27:48 +0530

    initial backend commit

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+//database
+const connectDB = async() =>{
+    try{
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log('MongoDB connection SUCCESS');
+        
+
+    }catch(err){
+        console.log(err);
+    }
+    
+}
+
+//middleware
+dotenv.config();
+
+app.listen(5000, () => {
+  connectDB() 
+  console.log('Server is running on port 5000');
+});
